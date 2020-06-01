40bc7cd47879f8c25507353a7767070281da0986
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-05-31 20:32:11 -0700

    added css and error handling

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import tempRequest
 
 app = Flask(__name__)
+
+app.secret_key = 'thisisasecretdonttellanyonepleasejustkidding'
 
 @app.route('/')
 def index():
@@ -10,7 +12,11 @@ def index():
 @app.route('/temperature', methods=['POST'])
 def temperature():
     if request.method == 'POST':
-        temp = tempRequest.temp(request.form['zip_code'], request.form['country'])
+        try:
+            temp = tempRequest.temp(request.form['zip_code'], request.form['country'])
+        except KeyError:
+            flash('Either zip-code or country was not found.', 'error')
+            return render_template('index.html')
     return render_template('temperature.html', currTemp=temp)
 
 if __name__ == '__main__':
